1e7408be2dd63702b36ba7d731b730f6918d318f
KDaryaS <dsk20071999@gmail.com>
2023-04-04 08:48:15 +0300

    Users GET/DELETE-request

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="userList_body">
-        <p class="userList_titleTable" id="titleTable">Зарегистированные пользователи</p>
+        <p class="userList_titleTable">Зарегистированные пользователи</p>
         <div class="userList_table">
-            <table class="userList_userTable" id="userList_userTable">
+            <table class="userList_userTable">
                 <thead class="userList_userTableHead">
                     <tr>
                         <th>Имя</th>
@@ -13,12 +13,15 @@
                     </tr>
                 </thead>
                 <tbody class="userList_userTableBody">
-                    <!-- <tr class="info">
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                    </tr> -->
+                    <tr class="userList_info" v-for="user in users" :key="user.id">
+                        <td>{{ user.firstName }}</td>
+                        <td>{{ user.lastName }}</td>
+                        <td>{{ user.email }}</td>
+                        <td>{{ user.phone }}</td>
+                        <td>
+                            <button @click="deleteUser(user.id, user.firstName, user.lastName)">Удалить</button>
+                        </td>
+                    </tr>
                 </tbody>
             </table>
         </div>
@@ -29,111 +32,102 @@
 export default {
     name: 'Statistics',
     data () {
-        return {
-            requestURL: 'http://192.168.31.74:8082/api/users',
-            xhr: new XMLHttpRequest()
-        }
+        return {users: null}
     },
     methods: {
-        sendRequest (method, url) {
-            return new Promise((resolve, reject) => {
-                this.xhr.open(method, url)
-                this.xhr.responseType='json'
-                this.xhr.setRequestHeader('Content-Type', 'application/json')
-                this.xhr.onload = () => {
-                    if(this.xhr.status>=400) {
-                        reject(this.xhr.response)
-                    } else {
-                        resolve(this.xhr.response)
-                    }
-                }
-                this.xhr.onerror = () => {
-                    reject(this.xhr.response)
-                }
-                this.xhr.send()
-            })
-        },
-        showUsers () {
-            this.sendRequest ('GET', this.requestURL)
-                .then(info => {
-                    // let div = document.createElement('div');
-                    // div.className = 'table'
-
-                    // let table = document.createElement('table');
-                    // table.className = 'userTable';
-
-                    // let thead = document.createElement('thead');
-                    // thead.className = 'userTableHead';
-
-                    // let tbody = document.createElement('tbody');
-                    // tbody.className = 'userTableBody';
-
-                    // titleTable.after(div);
-                    // div.appendChild(table)
-                    // table.appendChild(thead);
-                    // table.appendChild(tbody);
-
-                    // let row_1 = document.createElement('tr');
-                    // let heading_0 = document.createElement('th');
-                    // heading_0.innerHTML = 'Имя';
-                    // let heading_1 = document.createElement('th');
-                    // heading_1.innerHTML = "Фамилия";
-                    // let heading_2 = document.createElement('th');
-                    // heading_2.innerHTML = "Email";
-                    // let heading_3 = document.createElement('th');
-                    // heading_3.innerHTML = "Телефон";
-
-                    // row_1.appendChild(heading_0);
-                    // row_1.appendChild(heading_1);
-                    // row_1.appendChild(heading_2);
-                    // row_1.appendChild(heading_3);
-                    // thead.appendChild(row_1);
-
-
-                    let userTableBody = document.querySelector('tbody');
-                    for(let user of info) {
-                        let row = document.createElement('tr');
-                        row.className = 'userList_info';
-                        let td_0 = document.createElement('td');
-                        td_0.innerHTML = user.firstName;
-                        let td_1 = document.createElement('td');
-                        td_1.innerHTML = user.lastName;
-                        let td_2 = document.createElement('td');
-                        td_2.innerHTML = user.email;
-                        let td_3 = document.createElement('td');
-                        td_3.innerHTML = user.phone;
-                        let td_4 = document.createElement('td');
-                        td_4.innerHTML = '/';
-                        td_4.onclick = function AAA() {
-                            console.log(user.id)
-                            // this.sendRequest ('DELETE', this.requestURL)
-                            //     .then(data => console.log(data))
-                            //     .catch(err => console.log(err))
-                        };
-
-                        row.appendChild(td_0);
-                        row.appendChild(td_1);
-                        row.appendChild(td_2);
-                        row.appendChild(td_3);
-                        row.appendChild(td_4);
-                        
-                        userTableBody.appendChild(row)
-                    }
+        getData () {
+            this.axios.get('http://192.168.31.74:8082/api/users')
+                .then((result) => {
+                    this.users = result.data
                 })
-                .catch(err => console.log(err))
+                .catch((err) => console.log(err))
         },
+        deleteUser(id, firstName, lastName) {
+            let confirmation = confirm(`Вы уверены, что хотите удалить пользователя по имени: ${firstName} ${lastName}?`)
+            if (confirmation) {
+                this.axios.delete(`http://192.168.31.74:8082/api/users/delete?id=${id}`)
+                    .then(() => {
+                        this.getData()
+                    })
+                    .catch((err) => console.log(err))
+            } else {return}     
+        }
     },
     mounted() {
-        if (document.querySelectorAll('tr').length <= 1) {
-            this.showUsers ()
-        } else {
-            return
-        }
+        this.getData()
     }
 }
+
+
+// export default {
+//     name: 'Statistics',
+//     data () {
+//         return {
+//             requestURL: 'http://192.168.31.74:8082/api/users',
+//             xhr: new XMLHttpRequest()
+//         }
+//     },
+//     methods: {
+//         sendRequest (method, url) {
+//             return new Promise((resolve, reject) => {
+//                 this.xhr.open(method, url)
+//                 this.xhr.responseType='json'
+//                 this.xhr.setRequestHeader('Content-Type', 'application/json')
+//                 this.xhr.onload = () => {
+//                     if(this.xhr.status>=400) {
+//                         reject(this.xhr.response)
+//                     } else {
+//                         resolve(this.xhr.response)
+//                     }
+//                 }
+//                 this.xhr.onerror = () => {
+//                     reject(this.xhr.response)
+//                 }
+//                 this.xhr.send()
+//             })
+//         },
+//         showUsers () {
+//             this.sendRequest ('GET', this.requestURL)
+//                 .then(info => {
+//                     let userTableBody = document.querySelector('tbody');
+//                     for(let user of info) {
+//                         let row = document.createElement('tr');
+//                         row.className = 'userList_info';
+//                         let td_0 = document.createElement('td');
+//                         td_0.innerHTML = user.firstName;
+//                         let td_1 = document.createElement('td');
+//                         td_1.innerHTML = user.lastName;
+//                         let td_2 = document.createElement('td');
+//                         td_2.innerHTML = user.email;
+//                         let td_3 = document.createElement('td');
+//                         td_3.innerHTML = user.phone;
+//                         let td_4 = document.createElement('td');
+//                         td_4.innerHTML = '/';
+//                         };
+
+//                         row.appendChild(td_0);
+//                         row.appendChild(td_1);
+//                         row.appendChild(td_2);
+//                         row.appendChild(td_3);
+//                         row.appendChild(td_4);
+                        
+//                         userTableBody.appendChild(row)
+//                     }
+//                 })
+//                 .catch(err => console.log(err))
+//         },
+//     },
+//     mounted() {
+//         if (document.querySelectorAll('tr').length <= 1) {
+//             this.showUsers ()
+//         } else {
+//             return
+//         }
+//     }
+// }
 </script>
 
-<style > 
+<style scoped> 
 .userList_body {
     margin: 0;
     padding-top: 145px;
